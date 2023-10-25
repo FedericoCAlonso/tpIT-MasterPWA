72110f78a4a19aa7099ca0bcbe5ee292e6e97396
federicoCAlonso <federico.c.alonso@outlook.com.ar>
2023-10-25 20:49:49 -0300

    solución por modificacion de url v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-
+const urlStart = "tpIT-MasterPWA/";
 
 self.addEventListener("install", async event => {
     // sólo si queremos forzar al viejo SW a dejar de estar activo
@@ -6,8 +6,8 @@ self.addEventListener("install", async event => {
 
     // precache - al momento de instalar el SW, vamos a descargar archivos para servir luego
     const cache = await caches.open("recursos");
-    await cache.addAll(["/", "index.html", "assets/CSS/fonts.css","assets/CSS/navbar.css",
-    "assets/CSS/paleta.css","assets/CSS/style.css", "assets/iconos/logoLocoWeather_512.png","assets/iconos/logoSimple.png", 
+    await cache.addAll([urlStart+"/", urlStart+"index.html", urlStart+"assets/CSS/fonts.css",urlStart+"assets/CSS/navbar.css",
+    urlStart+"assets/CSS/paleta.css",urlStart+"assets/CSS/style.css", urlStart+"assets/iconos/logoLocoWeather_512.png",urlStart+"assets/iconos/logoSimple.png", 
         "https://fonts.googleapis.com/css2?family=Lobster&display=swap",
         "https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap"
     ]);
